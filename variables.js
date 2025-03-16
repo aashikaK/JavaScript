77b09fed442e176269d41a5720186559914243e0
Aashika <khatiwadaaashika12@gmail.com>
2025-03-16 18:29:39 +0545

    js

---
+++ variables.js
@@ -0,0 +1,13 @@
+var a=10;  //can be redeclared and updated
+console.log(a);
+
+var a="Aashika";
+console.log(a); //Aashika
+
+let b=4;   //canot be redeclared but updated
+console.log(b);
+//let b=9 will throw an error
+b="9";
+console.log(b);
+
+const pi=3.14; //cannot be redeclared or updated
