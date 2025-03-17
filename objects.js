80cee49c6f2ea45271c699209dc40353ade99da4
Aashika <khatiwadaaashika12@gmail.com>
2025-03-17 18:17:23 +0545

    js

---
+++ objects.js
@@ -0,0 +1,9 @@
+//non primitive data type
+
+const students={
+   " Rahul": true,
+    "Binod":667,
+    "Mohit": undefined
+};
+console.log(students[' Rahul']);
+console.log(students['kiran']); //undefined as this key s not there in the object
