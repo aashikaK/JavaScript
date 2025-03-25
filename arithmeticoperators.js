8a99753879f47b912b5ed09a67939a0a4bec94ad
Aashika <khatiwadaaashika12@gmail.com>
2025-03-25 18:18:25 +0545

    js

---
+++ arithmeticoperators.js
@@ -0,0 +1,10 @@
+let a=45;
+let b=4;
+console.log("a^b= "+ a**b); //shortcut for a^b
+console.log(a*b + " =a*b");
+console.log("a++= "+ a++);
+console.log(a);
+console.log("a-- = "+ a--);
+console.log(a);
+console.log("++a= "+ ++a);
+console.log("a%b= "+ a%b);
